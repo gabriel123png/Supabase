3822da25518010ea8755394bc7c9544bb54af80f
gabriel123png <gabrimh12@gmail.com>
2026-09-02 10:14:31 -0500

    supabase

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import ListaUsuarios from './components/ListaUsuarios'
+
+function App() {
+    return (
+        <div>
+            <h1>Mi app conectada a Supabase</h1>
+            <ListaUsuarios />
+        </div>
+    )
+}
+
+export default App
